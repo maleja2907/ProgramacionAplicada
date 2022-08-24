07c4642b082e3169ce00ef918903abe6958613bb
maleja2907 <110635635+maleja2907@users.noreply.github.com>
2022-08-23 21:51:27 -0500

    Update 12-TALLER01.py

--- CORTE01/12-TALLER01.py
+++ CORTE01/12-TALLER01.py
@@ -53,15 +53,29 @@ assert f_calBin (1.25) == 1.01
 
 ## 4. Valores estadisticos (1.5)
 import numpy as np
-def f_stat (l_valores):
-    '''
-    función que toma una lista de valores y retorna el promedio, la mediana y la desviación estandar
-    :param l_valores: lista con los valores a utilizar
-    :return:
-    '''
-    s_mean, s_median, s_STD = 0,0,0
-    #TODO: realizar las modificaciones necesarias a partir de esta sección
-    return s_mean,s_median,s_STD
+l_valores = [9,2, 9, 4, 5, 1, 8, 7, 8, 8, 9, 10,3]
+def f_stat(l_valores):
+    s_mean, s_median, s_STD = 0, 0, 0
+    print(l_valores)
+    int_contadora=0
+    int_divisor=len(l_valores)
+    for i in range (0,int_divisor):
+        int_contadora+=l_valores[i]
+    s_mean=int_contadora/int_divisor
+    if int_divisor%2==0:
+        s_median=((l_valores[(round((len(l_valores) / 2)+0.5))])+(l_valores[(round((len(l_valores) / 2)-1))]))/2
+    else:
+        s_median=l_valores[(round((len(l_valores) / 2)+0.5)-1)]
+    def mode(l_valores):
+      frequency = {}
+      for value in l_valores:
+          frequency[value] = frequency.get(value, 0) + 1
+      most_frequent = max(frequency.values())
+      s_moda = [key for key, value in frequency.items()
+                        if value == most_frequent]
+      return s_moda
+    return (f"la media es:{s_mean}, y la mediana es: {s_median}, y la moda es: {mode(l_valores)}")
+print(f_stat(l_valores))
 
 ## 5. BONO (0.5)
 #TODO: realizar la verificación del punto aterior haciendo uso de la función assert (pytest)
